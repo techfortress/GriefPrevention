e25ed9462fc16df21937f76535aca8016c56cd90
112madgamer <ryleegeorge1@gmail.com>
2018-11-03 13:03:28 +1100

    Allow the ClaimModifiedEvent to be cancelled

--- src/main/java/me/ryanhamshire/GriefPrevention/events/ClaimModifiedEvent.java
+++ src/main/java/me/ryanhamshire/GriefPrevention/events/ClaimModifiedEvent.java
@@ -3,6 +3,7 @@ package me.ryanhamshire.GriefPrevention.events;
 
 import me.ryanhamshire.GriefPrevention.Claim;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
@@ -11,14 +12,15 @@ import org.bukkit.event.HandlerList;
  * a claim has changed. The CommandSender can be null in the event that the modification is called by the plugin itself.
  * Created by Narimm on 5/08/2018.
  */
-public class ClaimModifiedEvent extends Event {
+public class ClaimModifiedEvent extends Event implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
-    
+    private boolean cancelled = false;
+
     public static HandlerList getHandlerList() {
         return handlers;
     }
-    
+
     private final Claim claim;
     private CommandSender modifier;
 
@@ -48,5 +50,17 @@ public class ClaimModifiedEvent extends Event {
      */
     public CommandSender getModifier() {
         return modifier;
+    }
+
+    @Override
+    public boolean isCancelled()
+    {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled)
+    {
+        this.cancelled = cancelled;
     }
 }
