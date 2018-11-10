d8ee4cd74a8c285b16793787474fc2957cc0d40c
112madgamer <ryleegeorge1@gmail.com>
2018-11-10 20:09:25 +1100

    Disable broken fixes and logic

--- src/main/java/me/ryanhamshire/GriefPrevention/RestoreNatureExecutionTask.java
+++ src/main/java/me/ryanhamshire/GriefPrevention/RestoreNatureExecutionTask.java
@@ -82,8 +82,9 @@ class RestoreNatureExecutionTask implements Runnable
 						
 						try
 						{
-						currentBlock.setType(blockUpdate.typeId, false);
-						currentBlock.setBlockData(blockUpdate.data, false);
+							currentBlock.setType(blockUpdate.typeId, false);
+
+							// currentBlock.setBlockData(blockUpdate.data, false);
 						}
 						catch(IllegalArgumentException e)
 						{
